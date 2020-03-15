f09541b156a7a37496e047daf35c3371ed3bdcd1
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 22:18:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[{:known_for => "Ruby",
-    :languages => ["LISP", "C"]
-}
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
+  
 end
 
 def changing_alan
@@ -105,8 +103,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  return programmer_hash
 end
